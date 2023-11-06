2e958cf61b040358b28423a5b6760e30f260cc83
alienshy <alisahverdiyew8@gmail.com>
2023-11-07 02:13:42 +0400

    task16

---
+++ script1.js
@@ -0,0 +1,45 @@
+//////////////<!-- 1. const array = [1,2,3,4,5]. Yeni array yaradın və bu array-in əksinə düzülmüş formada olsun (e.x [5,4,3,2,1])
+// const array = [1, 2, 3, 4, 5];
+// const yeniarray = [];
+// for (let i=array.length-1; i >= 0;i--){
+//     yeniarray.push(array[i]);
+// }
+// console.log(yeniarray);
+
+
+/////////////<!-- 2. const strArray = ["cat","mouse","panter","chicken"] bu array-dən "cat" sözünü silin və sonuna "bird" əlavə edin.
+// const strArray = ["cat", "mouse", "panter", "chicken"];
+// const konsulos = strArray.shift();
+// strArray.push("bird");
+// console.log(strArray);
+
+
+// /////////const array = [1,2,3,4,5]. Bu ədədlərin yanına kvadrat-ların əlavə edin. (e.x [1,2,3,4,5,1,4,9,16,25])
+// //////////1
+// const sarray = [1, 2, 3, 4, 5];
+// const array1 = [];
+// for (const item of sarray) {
+//     array1.push(item, item ** 2);
+// }
+// console.log(array1);
+
+
+///////// ////2
+// const array = [1, 2, 3, 4, 5];
+// const newArray = [];
+// for (let i = 0; i < 5; i++) {
+//     newArray.push(array[i], array[i] * array[i]);
+// }
+// console.log(newArray);
+
+
+// ////////4. Ədədlərdən ibarət array verilir. Array-də olan cüt ədədləri ekrana çıxardın.(Əlavə: yeni array-ə əlavə edin cüt ədədləri)
+// const array = [23,45,7,89,12,43,56,86,453,7,8786,,32,35,64,6];
+// const cutededler = [];
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 === 0) {
+//         cutededler.push(array[i]); 
+//     }
+// }
+// console.log("cüt ədədlər:", cutededler);
+
